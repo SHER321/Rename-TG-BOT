ad2fc184df382d9ef787b1f6627b1a44aff9889c
SHER321 <66766275+SHER321@users.noreply.github.com>
2020-06-18 18:30:29 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -7,24 +7,24 @@ SEE /help For My Features"""
     RENAME_403_ERR = "There Is Some Error With The File"
     ABS_TEXT = "What Are You Trying To Do,Mate?"
     UPGRADE_TEXT = "CONTACT @NGYNY"
-    DOWNLOAD_START = "Give Me Some Time Please 😴😴😴"
+    DOWNLOAD_START = "Downloding To My Server 📥 #NGY"
     UPLOAD_START = "Yay, Completed Downloading 😃😃😃"
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It? use @ALL_MOVIES_LIABRARY"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me,For Downloading Files Join @ALL_MOVIES_LIABRARY**"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds Use @ALL_MOVIES_LIABRARY To Download From Links"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It? use @NGY_BOTS"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me,For Downloading Files Join @NGY_BOTS**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds Use @NGY_BOTS To Download From Links"
     NOT_AUTH_USER_TEXT = "CONTACT @NGYNY"
     NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @NGYNY"
-    SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail Saved ✅ This Is Permanent Thumbnail ᕙ(  • ‿ •  )ᕗ "
+    SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail Saved ✅ This Is Permanent Thumbnail 😱😱😱 #NGY "
     DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfull🤦"
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "File Downloaded Successfully😎"
-    CUSTOM_CAPTION_UL_FILE = """@ALL_MOVIES_LIABRARY"""
+    CUSTOM_CAPTION_UL_FILE = """@NGY_BOTS"""
     HELP_USER = """<b>How To Use Me</b>
     1. Send me any Telegram File(Some Files Gets Error)
     2. Reply to that message to /rename NEW NAME.EXTENSION
     FEATURES
     👉Rename Telegram File With Custom Thumbnail.
     👉Convert Streamable File(Video) To File.
-    👉FOR Downloading Files At Highspeed Join @ALL_MOVIES_LIABRARY"""
+    👉FOR Downloading Files At Highspeed Join @NGY_BOTS"""
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply The New Name With /rename Filename.extn"
     IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
